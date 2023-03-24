2359047af6fcd89d873fb24d0beefc6256be81aa
Meghann Stamper <meg.stamp@yahoo.com>
2023-03-24 09:56:36 -0400

    basic loops

--- basics.js
+++ basics.js
@@ -1,13 +1,18 @@
 // Write a loop to print "0. Hello" to "9. Hello" (10 lines)
-
-// write loop here... 
-
-
+    //initilizing a variable
+for(let i = 0; i < 10; i++) { 
+    console.log(`${i}.Hello`)
+}
+    
 
 
 // Write a loop to count down from 10 to 1 (console.log)
 // and then write "Blast Off!"
 
-// write loop here... 
+for (let i = 10; i > 0; i--) {
+    console.log(i)
+}
+
+console.log("blast off!")
 
 
